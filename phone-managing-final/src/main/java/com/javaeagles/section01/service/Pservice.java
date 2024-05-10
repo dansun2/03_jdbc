f0b67b703fdc68ad00dd53fe3a7f3f7b80437951
dansun2 <dansunmusik7@gmail.com>
2024-05-10 16:45:38 +0900

    240510

---
+++ phone-managing-final/src/main/java/com/javaeagles/section01/service/Pservice.java
@@ -0,0 +1,21 @@
+package com.javaeagles.section01.service;
+
+import com.javaeagles.section01.dto.PhoneDTO;
+
+import java.util.ArrayList;
+
+public class Pservice {
+
+    public ArrayList<PhoneDTO> members = new ArrayList<>(); {
+
+    }
+
+    // 컨트롤이랑 파인더 받은 데이터를 관리
+    // 로직 수행 및 데이터 베이스에 대한 리소스를 관리
+    // 등록일 경우 등록해주기
+    // 수정일 경우 수정해주기
+    // 삭제일 경우 삭제해주기
+    // 전체조회
+    // 상세조회
+    // 레파지토리로 넘겨주기
+}
